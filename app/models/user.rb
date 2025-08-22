78e0e685d5c20c94c191da54fb0161dc2a1f6c19
manul <manuuva5@gmail.com>
2025-08-22 10:07:35 -0300

    add event creator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :created_events, class_name: "Event", foreign_key: "creator_id"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_21_130234) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_22_123814) do
   create_table "events", force: :cascade do |t|
     t.date "event_date"
     t.string "location"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_21_130234) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
